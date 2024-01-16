a6da649e3dcbcced60f4266853b44123b12a6eaa
Manul Thanura <manulkarunarathna@gmail.com>
2024-01-16 06:12:00 +0530

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,37 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-// 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-// 	theme: {
-// 		extend: {},
-// 	},
-// 	plugins: [],
-// }
-
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
-module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		screens: {
+		  sm: '480px',
+		  md: '768px',
+		  lg: '976px',
+		  xl: '1440px',
+		},
+		colors: {
+		  'blue': '#1fb6ff',
+		  'purple': '#7e5bef',
+		  'pink': '#ff49db',
+		  'orange': '#ff7849',
+		  'green': '#13ce66',
+		  'yellow': '#ffc82c',
+		  'gray-dark': '#273444',
+		  'gray': '#8492a6',
+		  'gray-light': '#d3dce6',
+		},
+		fontFamily: {
+		  sans: ['Graphik', 'sans-serif'],
+		  serif: ['Merriweather', 'serif'],
+		},
+		extend: {
+		  spacing: {
+			'128': '32rem',
+			'144': '36rem',
+		  },
+		  borderRadius: {
+			'4xl': '2rem',
+		  }
+		}
+	  },
+	plugins: [],
+}
