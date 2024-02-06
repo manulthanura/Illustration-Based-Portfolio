9f165158ed3175d69e2ba28d24c06e46b320570d
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-02-07 02:09:40 +0530

    feat: astro-font - optimize fonts & prevent CLS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,33 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		screens: {
-		  sm: '480px',
-		  md: '768px',
-		  lg: '976px',
-		  xl: '1440px',
-		},
-		colors: {
-			'black': '#000',
-			'white': '#fff',
-			'zinc': {
-				100: '#F8F8F8',
-				200: '#F0F0F0',
-				300: '#E8E8E8',
-				400: '#D8D8D8',
-				500: '#C8C8C8',
-				600: '#B0B0B0',
-				700: '#787878',
-				800: '#505050',
-				900: '#383838',
-			}
-		},
-		extend: {
-		  fontFamily: {
-			'Sora': ['Sora']
-		  }
-		}
-	  },
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      black: "#000",
+      white: "#fff",
+      zinc: {
+        100: "#F8F8F8",
+        200: "#F0F0F0",
+        300: "#E8E8E8",
+        400: "#D8D8D8",
+        500: "#C8C8C8",
+        600: "#B0B0B0",
+        700: "#787878",
+        800: "#505050",
+        900: "#383838",
+      },
+    },
+    extend: {
+      fontFamily: {
+        Sora: ["var(--font-sora)"],
+      },
+    },
+  },
+};
